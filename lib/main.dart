94ef3839b1dcf34834ef0159da4ee2b1a8fd3d04
Oluwadare Emmanuel <oluwadare.emmanuel15@gmail.com>
2023-10-22 14:16:28 +0100

    navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: MyApp(),
-  debugShowCheckedModeBanner: false,
-)
-
-
+    home: MyApp(),
+    debugShowCheckedModeBanner: false,
+  )
 );
 
 class MyApp extends StatelessWidget {
@@ -14,9 +12,31 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(icon: Icon(Icons.menu), onPressed: (){},),
-        title: Text('Buzzer'),
+      appBar: AppBar(title: Text('Buzzer')),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+              accountEmail: Text('FlutterDevDare@coding.com'),
+              accountName: Text('Dare'),
+              currentAccountPicture: CircleAvatar(backgroundColor: Colors.red,),
+
+            ),
+            ListTile(leading: Icon(Icons.home), title: Text('Home'), onTap: (){},),
+            ListTile(leading: Icon(Icons.shopping_bag), title: Text('Shop'), onTap: (){},),
+            ListTile(leading: Icon(Icons.favorite), title: Text('Favorites'), onTap: (){},),
+
+            Padding(
+              padding: const EdgeInsets.all(14.0),
+              child: Text('Labels'),
+            ),
+            ListTile(leading: Icon(Icons.label), title: Text('Red'), onTap: (){},),
+            ListTile(leading: Icon(Icons.label), title: Text('Green'), onTap: (){},),
+            ListTile(leading: Icon(Icons.label), title: Text('Blue'), onTap: (){},),
+
+          ],
+        ) ,
+        elevation: 140.0,
       ),
     );
   }
